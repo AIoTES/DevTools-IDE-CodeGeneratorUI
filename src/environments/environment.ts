3f9bad3911ae6d81be3e4bc4bc644d8bf57ac8c3
Buhid Eduardo <ebuhid@lst.tfo.upm.es>
2020-09-17 20:14:11 +0200

    added https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    baseUrl: 'http://localhost:4200',
+    baseUrl: 'https://localhost:4200',
     production: false,
-    codegenerator_url:"http://localhost:8181/GenerateCode",
+    codegenerator_url:"https://localhost:8181/GenerateCode",
     keycloakRealm: 'activage',
-    keycloakBaseUrl: 'http://activage-test.lst.tfo.upm.es:8081',
+    keycloakBaseUrl: 'https://activage-test.lst.tfo.upm.es:8081',
     CODEGENERATOR_PATH:window["env"]["CODEGENERATOR_PATH"] || "localhost:8181",
     AIOTES_HOSTNAME:window["env"]["AIOTES_HOSTNAME"] || "0",
     AIOTES_API_PORT:window["env"]["AIOTES_API_PORT"] || "0",
